affbd19783e10881e367970c78b79a59b11d824e
Voidane <hero4aquest@gmail.com>
2025-04-08 20:07:38 -0400

    Base of Displaying Networth

---
+++ ConfigData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+
+namespace NetworthTracker
+{
+    public class ConfigData
+    {
+        public static MelonPreferences_Category customNetworthPositioning;
+        public static MelonPreferences_Entry<bool> useCustomPosition;
+        public static MelonPreferences_Entry<float> anchorMinX;
+        public static MelonPreferences_Entry<float> anchorMinY;
+        public static MelonPreferences_Entry<float> anchorMaxX;
+        public static MelonPreferences_Entry<float> anchorMaxY;
+        public static MelonPreferences_Entry<float> offsetMinX;
+        public static MelonPreferences_Entry<float> offsetMinY;
+        public static MelonPreferences_Entry<float> offsetMaxX;
+        public static MelonPreferences_Entry<float> offsetMaxY;
+
+        public static MelonPreferences_Category presetNetworkPositioning;
+        public static MelonPreferences_Entry<bool> topLeft;
+        public static MelonPreferences_Entry<bool> topRight;
+        public static MelonPreferences_Entry<bool> bottomLeft;
+        public static MelonPreferences_Entry<bool> bottomRight;
+
+        public static ConfigData Instance;
+
+        public ConfigData()
+        {
+            Instance = this;
+
+            customNetworthPositioning = MelonPreferences.CreateCategory("UI Custom Positioning");
+            useCustomPosition = customNetworthPositioning.CreateEntry<bool>("Use Custom Positioning", false, "Use Custom Positioning",
+                "Allows the mod to configure the transform position of where you want the networth UI to be. (more precise positioning)");
+            anchorMinX = customNetworthPositioning.CreateEntry<float>("Anchor Min X", 0.8F);
+            anchorMinY = customNetworthPositioning.CreateEntry<float>("Anchor Min Y", 0.9F);
+            anchorMaxX = customNetworthPositioning.CreateEntry<float>("Anchor Max X", 1.0F);
+            anchorMaxY = customNetworthPositioning.CreateEntry<float>("Anchor Max Y", 1.0F);
+            offsetMinX = customNetworthPositioning.CreateEntry<float>("Offset Min X", 10.0F);
+            offsetMinY = customNetworthPositioning.CreateEntry<float>("Offset Min Y", 10.0F);
+            offsetMaxX = customNetworthPositioning.CreateEntry<float>("Offset Max X", -10.0F);
+            offsetMaxY = customNetworthPositioning.CreateEntry<float>("Offset Max Y", -10.0F);
+
+            presetNetworkPositioning = MelonPreferences.CreateCategory("UI Preset Positioning");
+            topLeft = presetNetworkPositioning.CreateEntry<bool>("Top Left", false);
+            topRight = presetNetworkPositioning.CreateEntry<bool>("Top Right", true);
+            bottomLeft = presetNetworkPositioning.CreateEntry<bool>("Bottom Left", false);
+            bottomRight = presetNetworkPositioning.CreateEntry<bool>("Bottom Right", false);
+
+            customNetworthPositioning.SetFilePath("UserData/NetworthTracker.cfg");
+            presetNetworkPositioning.SetFilePath("UserData/NetworthTracker.cfg");
+            customNetworthPositioning.SaveToFile();
+            presetNetworkPositioning.SaveToFile();
+        }
+    }
+}
---
+++ NetworthTrackerMod.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Il2CppScheduleOne.Money;
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NetworthTracker
+{
+    public class NetworthTrackerMod : MelonMod
+    {
+        public static Transform UI;
+        public static Transform HUD;
+        public static GameObject networthHolder;
+        public static Text networthDisplay;
+        public static bool mainSceneLoaded = false;
+        public static object networthCoroutineObject;
+
+        public static float networth;
+
+        public float Networth
+        {
+            set 
+            {
+                networth = value;
+                networthDisplay.text = $"Networth: ${value:N2}";
+            }
+            get => networth;
+        }
+
+        public override void OnInitializeMelon()
+        {
+            MelonLogger.Msg($"===========================================");
+            MelonLogger.Msg($"Initializing, Created by Voidane.");
+            MelonLogger.Msg($"Discord: discord.gg/XB7ruKtJje");
+            new ConfigData();
+            MelonLogger.Msg($"Has been initialized!");
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (sceneName == "Main")
+            {
+                mainSceneLoaded = true;
+                MelonLogger.Msg("Main scene loaded!");
+                MelonCoroutines.Start(WaitOnSceneLoad(null, "UI", 20.0F, (_UI) =>
+                {
+                    UI = _UI;
+                    MelonCoroutines.Start(WaitOnSceneLoad(UI, "HUD", 5.0F, (_HUD) =>
+                    {
+                        HUD = _HUD;
+                        CreateNetworthUI();
+                        networthCoroutineObject = MelonCoroutines.Start(UpdateNetworth());
+                    }));
+                    
+                }));
+            }
+            else
+            {
+                if (networthCoroutineObject != null)
+                {
+                    MelonCoroutines.Stop(networthCoroutineObject);
+                }
+
+                mainSceneLoaded = false;
+            }
+        }
+
+        private IEnumerator UpdateNetworth()
+        {
+            while (true)
+            {
+                MoneyManager money = MoneyManager.Instance;
+                MelonLogger.Msg("Cash: " + money.GetNetWorth());
+                Networth = (float) Math.Round(money.GetNetWorth(), 2);
+                yield return new WaitForSeconds(1.0F);
+            }
+        }
+
+        private IEnumerator WaitOnSceneLoad(Transform parent, string name, float timeoutLimit, Action<Transform> onComplete)
+        {
+            Transform target = null;
+            float timeOutCounter = 0F;
+            int attempt = 0;
+
+            MelonLogger.Msg($"Looking for {name} inside of {(parent == null ? "Hierarchy" : parent.gameObject.name)}");
+            while (target == null && timeOutCounter < timeoutLimit)
+            {
+                target = (parent == null) ? GameObject.Find(name).transform : parent.Find(name);
+                if (target == null)
+                {
+                    timeOutCounter += 0.5F;
+                    yield return new WaitForSeconds(0.5F);
+                }
+            }
+
+            if (target != null)
+            {
+                onComplete?.Invoke(target);
+            }
+            else
+            {
+                MelonLogger.Error("Failed to find target object within timeout period!");
+                onComplete?.Invoke(null);
+            }
+
+            yield return target;
+        }
+
+        private void CreateNetworthUI()
+        {
+            networthHolder = new GameObject("NetworthHolder");
+            networthHolder.transform.SetParent(HUD, false);
+            
+            RectTransform holderRect = networthHolder.AddComponent<RectTransform>();
+
+            if (ConfigData.Instance == null)
+            {
+                new ConfigData();
+            }
+
+            if (ConfigData.useCustomPosition.Value)
+            {
+                holderRect.anchorMin = new Vector2(ConfigData.anchorMinX.Value, ConfigData.anchorMinY.Value);
+                holderRect.anchorMax = new Vector2(ConfigData.anchorMaxX.Value, ConfigData.anchorMaxY.Value);
+                holderRect.offsetMin = new Vector2(ConfigData.offsetMinX.Value, ConfigData.offsetMinY.Value);
+                holderRect.offsetMax = new Vector2(ConfigData.offsetMaxX.Value, ConfigData.offsetMaxY.Value);
+            }
+            else if (ConfigData.topRight.Value)
+            {
+                holderRect.anchorMin = new Vector2(0.8f, 0.9f);
+                holderRect.anchorMax = new Vector2(1.0f, 1.0f);
+                holderRect.offsetMin = new Vector2(10f, 10f);
+                holderRect.offsetMax = new Vector2(-10f, -10f);
+            }
+            else if (ConfigData.topLeft.Value)
+            {
+                holderRect.anchorMin = new Vector2(0.0f, 0.9f);
+                holderRect.anchorMax = new Vector2(0.2f, 1.0f);
+                holderRect.offsetMin = new Vector2(10f, 10f);
+                holderRect.offsetMax = new Vector2(-10f, -10f);
+            }
+            else if (ConfigData.bottomLeft.Value)
+            {
+                holderRect.anchorMin = new Vector2(0.0f, 0.0f);
+                holderRect.anchorMax = new Vector2(0.2f, 0.1f);
+                holderRect.offsetMin = new Vector2(10f, 10f);
+                holderRect.offsetMax = new Vector2(-10f, -10f);
+            }
+            else if (ConfigData.bottomRight.Value)
+            {
+                holderRect.anchorMin = new Vector2(0.8f, 0.0f);
+                holderRect.anchorMax = new Vector2(1.0f, 0.1f);
+                holderRect.offsetMin = new Vector2(10f, 10f);
+                holderRect.offsetMax = new Vector2(-10f, -10f);
+            }
+
+            GameObject networthDisplayGameobject = new GameObject("NetworthText");
+            networthDisplayGameobject.transform.SetParent(networthHolder.transform, false);
+
+            networthDisplay = networthDisplayGameobject.AddComponent<Text>();
+            networthDisplay.text = "Networth: SYNCING";
+            networthDisplay.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+            networthDisplay.fontSize = 18;
+            networthDisplay.alignment = TextAnchor.MiddleCenter;
+            networthDisplay.color = Color.green;
+
+            RectTransform textRect = networthDisplayGameobject.GetComponent<RectTransform>();
+            textRect.anchorMin = Vector2.zero;
+            textRect.anchorMax = Vector2.one;
+            textRect.offsetMin = Vector2.zero;
+            textRect.offsetMax = Vector2.zero;
+        }
+    }
+}
